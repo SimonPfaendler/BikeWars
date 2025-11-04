834b2a45ebb1e2dea59bc14393d6293f683a59fc
simonp <simon-kirchzarten@web.de>
2025-11-04 17:58:48 +0100

    Player Rechteck + Tastaturbewegung (WASD). Closed #63

--- BikeWars/Game1.cs
+++ BikeWars/Game1.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using BikeWars.Components;
+using BikeWars.Entities.Characters;
+using BikeWars.Systems;
 
 namespace BikeWars;
 
@@ -11,11 +14,18 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Texture2D _texture;
 
+    Player player;
+    MovementSystem movement;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        // Just for Testing
+        _graphics.PreferredBackBufferWidth = 1280;
+        _graphics.PreferredBackBufferHeight = 720;
     }
 
     protected override void Initialize()
@@ -27,12 +37,21 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         //texture = Content.Load<Texture2D>("myimage");
+
+        // Get screen dimensions
+        int width = _graphics.PreferredBackBufferWidth;
+        int height = _graphics.PreferredBackBufferHeight;
+
+        // Spawn player in center of screen
+        player = new Player(new Vector2(width / 2, height / 2), new Point(32, 32));
+        movement = new MovementSystem(player);
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+        movement.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -43,6 +62,7 @@ public class Game1 : Game
 
         _spriteBatch.Begin();
         // spriteBatch.Draw(texture, new Vector2(100, 100), Color.White);
+        player.Draw(_spriteBatch);
         _spriteBatch.End();
 
         base.Draw(gameTime);
