7f4adc36c7a2441985123885f644fad5d2bf6a9a
jl589 <jl589@email.uni-freiburg.de>
2025-11-04 12:21:13 +0100

    Created the project. Added the first simple ideas and the folder structure to work it. Can still be changed

---
+++ BikeWars/Game1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BikeWars;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _texture;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        //texture = Content.Load<Texture2D>("myimage");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        // spriteBatch.Draw(texture, new Vector2(100, 100), Color.White);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ BikeWars/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new BikeWars.Game1();
+game.Run();
