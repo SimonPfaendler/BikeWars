7290d1fdbd673fec291f8a58986d209f5bdc0feb
Daniel <db489@email.uni-freiburg.de>
2025-11-29 16:50:09 +0100

    Clean Code siehe Issue165 Kommentare

--- BikeWars/Content/src/screens/OptionScreen.cs
+++ BikeWars/Content/src/screens/OptionScreen.cs
@@ -86,15 +86,7 @@ public class OptionScreen : MenuScreenBase, IScreen
             ));
             
         }
-    private Texture2D CreateSimpleTexture(GraphicsDevice graphicsDevice, int width, int height)
-    {
-        Texture2D texture = new Texture2D(graphicsDevice, width, height);
-        Color[] data = new Color[width * height];
-        for (int i = 0; i < data.Length; i++) 
-            data[i] = Color.White;
-        texture.SetData(data);
-        return texture;
-    }
+
         
     protected override void HandleButtonClick(MenuButton button)
     {
