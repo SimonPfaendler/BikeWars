453dea7bd999b645d1e27feabaaa012c03ed516f
SimonGenovese <simon.genovese@email.uni-freiburg.de>
2026-01-01 18:49:46 +0100

    Lautstärke für Musik und Soundeffekte ist jetzt einstellbar, bisher allerdings nur mit der Maus (#279)

---
+++ BikeWars/Content/src/screens/SoundConfigScreen.cs
@@ -0,0 +1,140 @@
+using System;
+using BikeWars.Content.engine.Audio;
+using BikeWars.Content.components;
+using BikeWars.Content.engine.interfaces;
+using BikeWars.Content.managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BikeWars.Content.screens
+{
+    public class SoundConfigScreen : MenuScreenBase, IScreen
+    {
+        private readonly AudioService _audioService;
+        
+        private Rectangle _musicTrackRect;
+        private Rectangle _sfxTrackRect;
+        
+        private Texture2D _sliderTexture;
+        
+        private bool _isDraggingMusic;
+        private bool _isDraggingSfx;
+        
+        public string DesiredMusic => AudioAssets.MenuMusic;
+        public float MusicVolume => 1f;
+
+        public SoundConfigScreen(Texture2D background, SpriteFont font, AudioService audioService)
+            : base(background, font)
+        {
+            _audioService = audioService;
+            
+            _buttonTexture = CreateSimpleTexture(Game1.Instance.GraphicsDevice, 1, 1);
+            _sliderTexture = CreateSimpleTexture(Game1.Instance.GraphicsDevice, 1, 1);
+            
+            InitializeButtons();
+        }
+
+        protected sealed override void InitializeButtons()
+        {
+            _buttons.Clear();
+            var viewport = Game1.Instance.GraphicsDevice.Viewport;
+            
+            int btnWidth = 200;
+            int btnHeight = 50;
+            int margin = 40;
+            
+            _buttons.Add(new MenuButton(
+                id: (int)ButtonAction.Back,
+                texture: _buttonTexture,
+                bounds: new Rectangle(margin, viewport.Height - btnHeight - margin, btnWidth, btnHeight),
+                text: "Back",
+                font: _font,
+                audioService: _audioService
+            ));
+            
+            int trackWidth = (int)(viewport.Width * 0.3f);
+            int trackHeight = 16;
+            int sliderY = (int)(viewport.Height * 0.4f);
+            
+            _musicTrackRect = new Rectangle(viewport.Width / 3 - trackWidth / 2, sliderY, trackWidth, trackHeight);
+            
+            _sfxTrackRect = new Rectangle(2 * viewport.Width / 3 - trackWidth / 2, sliderY, trackWidth, trackHeight);
+            UpdateSelection(0);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            MouseState mouse = Mouse.GetState();
+            Point mousePos = mouse.Position;
+            
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                // Music Volume
+                if (_musicTrackRect.Contains(mousePos) || _isDraggingMusic)
+                {
+                    _isDraggingMusic = true;
+                    float newValue = MathHelper.Clamp((float)(mousePos.X - _musicTrackRect.X) / _musicTrackRect.Width, 0f, 1f);
+                    _audioService.Music.MasterVolume = newValue;
+                }
+                
+                // Sound Effect Volume
+                if (_sfxTrackRect.Contains(mousePos) || _isDraggingSfx)
+                {
+                    _isDraggingSfx = true;
+                    float newValue = MathHelper.Clamp((float)(mousePos.X - _sfxTrackRect.X) / _sfxTrackRect.Width, 0f, 1f);
+                    _audioService.Sounds.MasterVolume = newValue;
+                }
+            }
+            else
+            {
+                _isDraggingMusic = false;
+                _isDraggingSfx = false;
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            
+            var spriteBatch = Game1.Instance.SpriteBatch;
+            spriteBatch.Begin();
+            
+            DrawSlider(spriteBatch, _musicTrackRect, _audioService.Music.MasterVolume, "Musik");
+            DrawSlider(spriteBatch, _sfxTrackRect, _audioService.Sounds.MasterVolume, "Sound Effekte");
+
+            spriteBatch.End();
+        }
+
+        private void DrawSlider(SpriteBatch sb, Rectangle track, float volume, string label)
+        {
+            sb.Draw(_sliderTexture, track, Color.Gray * 0.5f);
+            
+            // knob for sliding
+            int knobWidth = 24;
+            int knobHeight = 40;
+            int knobX = track.X + (int)(track.Width * volume) - (knobWidth / 2);
+            Rectangle knobRect = new Rectangle(knobX, track.Y + (track.Height / 2) - (knobHeight / 2), knobWidth, knobHeight);
+            
+            sb.Draw(_sliderTexture, knobRect, Color.Gold);
+
+            // label and percent value
+            string text = $"{label}: {(int)(volume * 100)}%";
+            Vector2 textSize = _font.MeasureString(text);
+            sb.DrawString(_font, text, new Vector2(track.Center.X - (textSize.X * 1.2f) / 2, track.Y - 50), Color.White, 0f, Vector2.Zero, 1.2f, SpriteEffects.None, 0f);
+        }
+
+        protected override void HandleButtonClick(MenuButton button)
+        {
+            if (button.Id == (int)ButtonAction.Back)
+            {
+                ScreenManager.RemoveScreen(this);
+            }
+        }
+        
+        public override bool DrawLower => false;
+        public override bool UpdateLower => false;
+    }
+}
