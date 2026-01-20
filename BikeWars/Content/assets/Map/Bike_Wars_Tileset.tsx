057168a92d355bc8fa56754f21cfb71b4a64b734
Fabian Gauss <fg66@email.uni-freiburg.de>
2026-01-20 14:07:07 +0100

    Added larger tilemap with new Objects

--- BikeWars/Content/assets/Map/Bike_Wars_Tileset.tsx
+++ BikeWars/Content/assets/Map/Bike_Wars_Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Bike_Wars_Tileset" tilewidth="16" tileheight="16" tilecount="8649" columns="93">
- <image source="tilemap_1.png" trans="ff00ff" width="1500" height="1500"/>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Bike_Wars_Tileset" tilewidth="16" tileheight="16" tilecount="24336" columns="156">
+ <image source="tilemap_1.png" trans="ff00ff" width="2500" height="2500"/>
 </tileset>
