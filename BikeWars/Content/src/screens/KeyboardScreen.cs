2eabfa259ba368eb11c41173d57b0b4a7575c9d6
SimonGenovese <simon.genovese@email.uni-freiburg.de>
2026-01-20 10:45:26 +0100

    Screens für Controller- und Tastatursteuerung (tastenbelegung) hinzugefügt (#280), Bilder mit tastenbelgung fehlen noch

---
+++ BikeWars/Content/src/screens/KeyboardScreen.cs
@@ -0,0 +1,66 @@
+using System;
+using BikeWars.Content.engine.interfaces;
+using BikeWars.Content.components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using BikeWars.Content.engine.Audio;
+using BikeWars.Content.managers;
+
+namespace BikeWars.Content.screens;
+
+public class KeyboardScreen: MenuScreenBase, IScreen
+{
+    private readonly AudioService _audioService;
+    public string DesiredMusic => AudioAssets.MenuMusic;
+    public float MusicVolume => 1f;
+    public KeyboardScreen(Texture2D background, SpriteFont font, AudioService audioService)
+        : base(background, font)
+    {
+        _audioService = audioService;
+        InitializeButtons();
+    }
+    
+    protected sealed override void InitializeButtons()
+        {
+            Game1 game = Game1.Instance;
+            int screenWidth = game.GraphicsDevice.Viewport.Width;
+            int screenHeight = game.GraphicsDevice.Viewport.Height;
+    
+            int buttonWidth = 250;
+            int buttonHeight = 60;
+            int verticalSpacing = 20;
+            int horizontalSpacing = screenWidth / 15;
+            
+            int leftStartY = screenHeight / 7;
+
+            _buttonTexture = CreateSimpleTexture(game.GraphicsDevice, buttonWidth, buttonHeight);
+
+            // Buttons on the left side
+
+            _buttons.Add(new MenuButton(
+                id: (int)ButtonAction.Back,
+                texture: _buttonTexture,
+                bounds: new Rectangle(horizontalSpacing, leftStartY + 3 * (buttonHeight + verticalSpacing), buttonWidth, buttonHeight),
+                text: "Back",
+                font: _font,
+                audioService: _audioService
+            ));
+            
+            UpdateSelection(0);
+        }
+
+        
+    protected override void HandleButtonClick(MenuButton button)
+    {
+        switch ((ButtonAction)button.Id)
+        {
+            case ButtonAction.Back:
+                ScreenManager.RemoveScreen(this);
+                break;
+        }
+    }
+    public override bool DrawLower => false;
+    public override bool UpdateLower => false;
+}
